3ab43031f96820cc9f761ee4d485dd444ae089ba
Andrew Vasilchenko <cazonove@gmail.com>
2025-04-02 14:59:52 +0300

    добавил aos.js

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 import './components/header.js';
+import './components/aos.js';
 import './components/offcanvas.js';
 import './components/transfer.js';
---
+++ src/js/components/aos.js
@@ -0,0 +1,6 @@
+import AOS from 'aos'
+
+AOS.init({
+  startEvent: 'load',
+  easing: 'ease-in-out',
+})
